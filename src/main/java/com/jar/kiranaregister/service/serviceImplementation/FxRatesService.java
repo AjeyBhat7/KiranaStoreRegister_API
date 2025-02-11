0c951a8f8a12c659e410334c3f205928504e869f
Ajey <ajeybhat@changejar.in>
2025-02-11 23:35:25 +0530

    Added cacheing to FxRates

--- src/main/java/com/jar/kiranaregister/service/serviceImplementation/FxRatesService.java
+++ src/main/java/com/jar/kiranaregister/service/serviceImplementation/FxRatesService.java
@@ -1,22 +1,45 @@
 package com.jar.kiranaregister.service.serviceImplementation;
 
+
 import com.jar.kiranaregister.model.responseModel.FxRatesResponse;
+import com.jar.kiranaregister.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class FxRatesService {
+
     private final RestTemplate restTemplate;
+    private final CacheService cacheService;
 
     @Autowired
-    public FxRatesService(RestTemplate restTemplate) {
+    public FxRatesService(RestTemplate restTemplate, CacheService cacheService) {
         this.restTemplate = restTemplate;
+        this.cacheService = cacheService;
     }
 
     public FxRatesResponse getLatestFxRates() {
         String url = "https://api.fxratesapi.com/latest";
+        String cacheKey = "FxRates";
 
-        return restTemplate.getForObject(url, FxRatesResponse.class);
+
+
+        if (cacheService.checkKeyExists(cacheKey)) {
+            String fxRatesResponseStr = cacheService.getValueFromRedis(cacheKey);
+
+            return  StringUtils.fromJson(fxRatesResponseStr, FxRatesResponse.class);
+        }
+
+        FxRatesResponse fxRatesResponse = restTemplate.getForObject(url, FxRatesResponse.class);
+
+        if (fxRatesResponse != null) {
+            String fxRatesResponseStr = StringUtils.toJson(fxRatesResponse);
+            cacheService.setValueToRedis(cacheKey, fxRatesResponseStr, 300L);
+        }
+
+        return fxRatesResponse;
     }
+
+
 }
