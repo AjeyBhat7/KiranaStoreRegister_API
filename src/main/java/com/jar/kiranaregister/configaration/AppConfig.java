fb3ceb6d661c183df3d33e0c40374d669672c876
Ajey <ajeybhat@changejar.in>
2025-02-11 23:36:37 +0530

    AppConfig

--- src/main/java/com/jar/kiranaregister/configaration/AppConfig.java
+++ src/main/java/com/jar/kiranaregister/configaration/AppConfig.java
@@ -6,8 +6,23 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
+
+
     @Bean
-    public RestTemplate restTemplate() {
+    public RestTemplate restTemplateBean() {
         return new RestTemplate();
     }
+
+//    @Bean
+//    public RestTemplate fxRatesRestTemplate() {
+//
+////        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+////        requestFactory.setConnectTimeout(5000);
+////        requestFactory.setConnectionRequestTimeout(5000);
+//        RestTemplate restTemplate = new RestTemplate();
+////        restTemplate.setObservationRegistry(this.observationRegistry);
+//
+//        return restTemplate;
+//
+//    }
 }
