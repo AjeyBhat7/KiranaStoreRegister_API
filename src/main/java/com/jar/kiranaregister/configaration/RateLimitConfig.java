e923d6a1afe692ae03079cf3f404b08c85b8409b
Ajey <ajeybhat@changejar.in>
2025-02-12 23:19:52 +0530

    Added configs related to rate limiting.

---
+++ src/main/java/com/jar/kiranaregister/configaration/RateLimitConfig.java
@@ -0,0 +1,27 @@
+package com.jar.kiranaregister.configaration;
+
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Refill;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.time.Duration;
+
+@Configuration
+public class RateLimitConfig {
+
+    @Bean
+    public Bucket defaultRateLimitBucket() {
+        return Bucket4j.builder()
+                .addLimit(Bandwidth.classic(1, Refill.greedy(1, Duration.ofSeconds(1))))
+                .build();
+    }
+
+    @Bean("FxRatesRateLimitBucket")
+    public Bucket FxRatesRateLimitBucket() {
+        return Bucket4j.builder()
+                .addLimit(Bandwidth.classic(5, Refill.greedy(5, Duration.ofSeconds(1))))
+                .build();
+    }
+}
