a2201a4b3b9e0b932619f2f1444b59b2a14ba239
Ajey <ajeybhat@changejar.in>
2025-02-13 13:06:17 +0530

    added error handling

--- src/main/java/com/jar/kiranaregister/service/serviceImplementation/FxRatesService.java
+++ src/main/java/com/jar/kiranaregister/service/serviceImplementation/FxRatesService.java
@@ -1,46 +1,49 @@
 package com.jar.kiranaregister.service.serviceImplementation;
 
-
 import com.jar.kiranaregister.AOP.RateLimited;
 import com.jar.kiranaregister.model.responseModel.FxRatesResponse;
 import com.jar.kiranaregister.utils.StringUtils;
+import io.github.bucket4j.Bucket;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClientException;
 
 @Service
 public class FxRatesService {
 
     private final RestTemplate restTemplate;
     private final CacheService cacheService;
-
+    
     @Autowired
     public FxRatesService(RestTemplate restTemplate, CacheService cacheService) {
         this.restTemplate = restTemplate;
         this.cacheService = cacheService;
+       
     }
 
-
-    @RateLimited(bucketQualifier = "FxRatesRateLimitBucket")
+    @RateLimited(bucketName = "FxRatesRateLimitBucket")
     public FxRatesResponse getLatestFxRates() {
-        String url = "https://api.fxratesapi.com/latest";
-        String cacheKey = "FxRates";
+        final String url = "https://api.fxratesapi.com/latest";
+       final String cacheKey = "FxRates";
 
-        if (cacheService.checkKeyExists(cacheKey)) {
-            String fxRatesResponseStr = cacheService.getValueFromRedis(cacheKey);
+        try {
+            if (cacheService.checkKeyExists(cacheKey)) {
+                String fxRatesResponseStr = cacheService.getValueFromRedis(cacheKey);
+                return StringUtils.fromJson(fxRatesResponseStr, FxRatesResponse.class);
+            }
 
-            return  StringUtils.fromJson(fxRatesResponseStr, FxRatesResponse.class);
+            FxRatesResponse fxRatesResponse = restTemplate.getForObject(url, FxRatesResponse.class);
+
+            if (fxRatesResponse != null) {
+                String fxRatesResponseStr = StringUtils.toJson(fxRatesResponse);
+                cacheService.setValueToRedis(cacheKey, fxRatesResponseStr, 300L);
+            }
+
+            return fxRatesResponse;
+        } catch (RestClientException e) {
+            return null;
         }
-
-        FxRatesResponse fxRatesResponse = restTemplate.getForObject(url, FxRatesResponse.class);
-
-        if (fxRatesResponse != null) {
-            String fxRatesResponseStr = StringUtils.toJson(fxRatesResponse);
-            cacheService.setValueToRedis(cacheKey, fxRatesResponseStr, 300L);
-        }
-
-        return fxRatesResponse;
     }
-
-
 }
